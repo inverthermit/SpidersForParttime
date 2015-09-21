2ca8c099f59877fe59b14e13b43e1b173187b79f
inverthermit <l1111j@live.com>
2015-09-21 23:06:21 +0800

    asdf

--- httpclienttest/src/bristol/BristolUnData.java
+++ httpclienttest/src/bristol/BristolUnData.java
@@ -9,7 +9,10 @@ public class BristolUnData {
 		// TODO Auto-generated method stub
 
 		//<div id="course-structure" class="module">
-		//
+		//id=typical-offer
+		//A 7.5 7  B D 7.0 7 C 6.5 6.5    E 6.5 6 F 6 6 Profile Foundation 6.0 5.5
+		//Arts courses 15200  Science courses 18300  Clinical courses 33900
+		System.out.println(("123,1241,1512,42 132,14212,24124,1241 12,3241,5125 asfd,fa12,412").replace("^[0-9]+,[0-9]+$", "eee"));
 	}
 
 }
