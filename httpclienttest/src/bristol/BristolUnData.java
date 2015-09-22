f7a77221892acd2849a33aace80e1cc1e0ed5132
unknown <l1111j@live.com>
2015-09-22 16:46:49 +0800

    bristol

--- httpclienttest/src/bristol/BristolUnData.java
+++ httpclienttest/src/bristol/BristolUnData.java
@@ -1,4 +1,7 @@
 package bristol;
+import java.util.*;
+import java.util.Comparator;
+import java.util.regex.*;
 
 public class BristolUnData {
 
@@ -11,8 +14,24 @@ public class BristolUnData {
 		//<div id="course-structure" class="module">
 		//id=typical-offer
 		//A 7.5 7  B D 7.0 7 C 6.5 6.5    E 6.5 6 F 6 6 Profile Foundation 6.0 5.5
-		//Arts courses 15200  Science courses 18300  Clinical courses 33900
-		System.out.println(("123,1241,1512,42 132,14212,24124,1241 12,3241,5125 asfd,fa12,412").replace("^[0-9]+,[0-9]+$", "eee"));
+		//Undergraduate fee:Arts courses 15200  Science courses 18300  Clinical courses 33900
+		//Postgraduate fee using EL pattern
+		Pattern p = Pattern.compile("[0-9]+,[0-9]+");
+		Matcher m = p.matcher("<dl><dt>UK/EU</dt><dd>£4,145</dd><dt>Overseas</dt><dd>£14,200</dd></dl>");
+		ArrayList<Integer> money=new ArrayList<Integer>();
+		while (m.find()) {
+			money.add(Integer.parseInt(m.group().replace(",", "")));
+		}
+		int max=0;
+		for(int w=0;w<money.size();w++)
+		{
+			if(money.get(w)>max)
+			{
+				max=money.get(w);
+			}
+		}
+		System.out.println(max);
+		
 	}
 
 }
